f9910baf493613b20feecf3843184088ffc2d606
Isak Horvath <isakhorvath@gmail.com>
2022-08-10 17:26:16 +0200

    FIX: javascript include NEW: add markup files

--- include.js
+++ include.js
@@ -5,7 +5,7 @@ function includeHTML() {
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    file = elmnt.getAttribute("include-html");
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
@@ -14,7 +14,7 @@ function includeHTML() {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
+          elmnt.removeAttribute("include-html");
           includeHTML();
         }
       }
